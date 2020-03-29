defb2ad87651d94179a281311e190995b4919004
Your Name <you@example.com>
2020-03-29 09:15:13 -0300

    Migrates

--- core_banking/start.sh
+++ core_banking/start.sh
@@ -5,8 +5,7 @@ while ! nc -z db 5432; do
 	sleep 5;
 done
 
-#prod/rel/prod/bin/prod eval mix ecto.create
-#prod/rel/prod/bin/prod eval mix ecto.migrate
+_build/prod/rel/prod/bin/prod eval Release.Tasks.create_and_migrate
 
 _build/prod/rel/prod/bin/prod start
 
