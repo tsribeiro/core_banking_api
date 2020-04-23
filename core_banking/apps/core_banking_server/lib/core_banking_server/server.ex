b5978bbb53c61bfc75e90b2e099c3e9243390224
Thiago Ribeiro <tsribeiro@stefanini.com>
2020-04-23 15:53:38 -0300

    healthcheck

--- core_banking/apps/core_banking_server/lib/core_banking_server/server.ex
+++ core_banking/apps/core_banking_server/lib/core_banking_server/server.ex
@@ -8,6 +8,10 @@ defmodule CoreBankingServer.Server do
 
   forward("/accounts", to: CoreBankingServer.Account)
 
+  get "/" do
+    send_resp(conn, 200, "ok")
+  end
+
   match _ do
     send_resp(conn, 404, "Not Found Resource")
   end
